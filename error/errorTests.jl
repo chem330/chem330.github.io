ee6b5586458c60296cc13da5802d7b08cc4f1480
Al Fischer <46463825+dr-fischer@users.noreply.github.com>
2023-08-25 15:40:44 -0400

    Update errorTests.jl

--- error/errorTests.jl
+++ error/errorTests.jl
@@ -19,7 +19,7 @@ function check_input_sf(X, glassware; balanceType = nothing, debouce = 2)
 		end
 	
 		if any(.!(ismissing.(X))) && all(length.(string.(X)) .!= sig+1)
-		Markdown.MD(Markdown.Admonition("danger", "Heads Up!", [md"Remember to record all known digits plus one uncertain digit for all measurements!"]))
+			Markdown.MD(Markdown.Admonition("danger", "Heads Up!", [md"Remember to record all known digits plus one uncertain digit for all measurements!"]))
 		end
 	
 	catch
