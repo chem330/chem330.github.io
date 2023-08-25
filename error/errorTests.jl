91b6099b29908352fe828defee7021d2387f5b97
Al Fischer <46463825+dr-fischer@users.noreply.github.com>
2023-08-25 15:42:45 -0400

    Update errorTests.jl

--- error/errorTests.jl
+++ error/errorTests.jl
@@ -5,7 +5,7 @@ function check_input_sf(X, glassware; balanceType = nothing, debouce = 2)
 		if glassware == "balance" && balanceType == "A"
 			sig = 6
 		elseif glassware == "balance" && balanceType == "choose one..."
-			Markdown.MD(Markdown.Admonition("danger", "Choose the correct balance!", [md"There are several types of balances in use.  Yours should be marked with an A or B.  Make sure to choose the correct one from the dropdown."]))	
+			Markdown.MD(Markdown.Admonition("danger", "Choose the correct balance!", [md"Choose the right balance in the dropdown menu above based on the label on the one you are using."]))	
 		elseif glassware == "balance" && balanceType == "B"
 			sig = 5
 		elseif glassware == "beaker"
