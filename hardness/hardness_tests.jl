920b02637800e3955ba436c7c9d5223b47b4d27d
Al Fischer <46463825+dr-fischer@users.noreply.github.com>
2023-09-12 16:15:15 -0400

    Update hardness_tests.jl

--- hardness/hardness_tests.jl
+++ hardness/hardness_tests.jl
@@ -53,7 +53,7 @@ function check_edta_conc(check_string, mass, volume)
 end
 function check_ca_amount(check_string, stock_vol, stock_conc)
 	if !ismissing(stock_vol) && check_string != "0.00"
-		if isapprox(parse(Float64, check_string), stock_conc * stock_vol, atol = 0.01)
+		if isapprox(parse(Float64, check_string), stock_conc * stock_vol * 0.5, atol = 0.01)
 			Markdown.MD(Markdown.Admonition("correct", "Correct!", [md"Your calculation is correct!"]))
 		else
 			Markdown.MD(Markdown.Admonition("danger", "This doesn't seem right...", [md"Something's wrong with your calculation."]))
