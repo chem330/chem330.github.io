ad6065dd5b47814a43832e3802f208b47a727796
Al Fischer <46463825+dr-fischer@users.noreply.github.com>
2023-09-17 13:41:17 -0400

    Update hardness_tests_fass.jl

--- hardness/hardness_tests_fass.jl
+++ hardness/hardness_tests_fass.jl
@@ -7,7 +7,7 @@ end
   
 function check_dilutions(X, stock, vf, pip; species, a = 0.00001)
   if all(.!ismissing.(vf)) && all(.!ismissing.(pip)) && all(.!ismissing.(X))
-    if all(isapprox.(X, stock .* pip ./ vf, atol = a)) || all(isapprox.(reverse(X), stock .* pip ./ vf), atol = a) 
+    if all(isapprox.(X, stock .* pip ./ vf, atol = a)) || all(isapprox.(reverse(X), stock .* pip ./ vf, atol = a)) 
       Markdown.MD(Markdown.Admonition("correct", "Correct!", [md"Your $(species) standard concentrations appear to be correct!"]))
     else
       Markdown.MD(Markdown.Admonition("danger", "This doesn't seem right...", [md"One or more of your $(species) standard concentrations is incorrect."]))
