c5e69833d4d344164a454e19a1b7553693b42852
Al Fischer <46463825+dr-fischer@users.noreply.github.com>
2023-09-27 15:28:09 -0400

    Update nitrate-tests.jl

--- nitrate/nitrate-tests.jl
+++ nitrate/nitrate-tests.jl
@@ -1,6 +1,7 @@
 [
 function setstockconcs()
-  	nitrate_stock = 118.4 # mg/L
+  	# nitrate_stock = 118.4 # mg/L
+	nitrate_stock = stock_nitrate_conc
 	qc = 7.696 # mg/L
   	return (n = nitrate_stock, qc = qc)
 end
